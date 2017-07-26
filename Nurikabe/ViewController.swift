4e8661176044a2762834c3ce0434fd7728771cc1
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2017-07-26 15:23:58 -0700

    Initial Commit

---
+++ Nurikabe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nurikabe
+//
+//  Created by JIAWEI CHEN on 7/26/17.
+//  Copyright © 2017 Pokgear Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
