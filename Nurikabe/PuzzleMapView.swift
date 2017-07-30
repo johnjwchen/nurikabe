077616d21c1efb0d601aebad0678fe77217b29b8
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2017-07-29 18:30:11 -0700

    puzzlemapview works. need test

--- Nurikabe/PuzzleMapView.swift
+++ Nurikabe/PuzzleMapView.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+fileprivate enum RetangleValue {
+    case black
+    case white
+    case value(Int)
+}
+
 class PuzzleMapLayer: CAShapeLayer {
     var rows: Int = 0 {
         didSet {
@@ -34,6 +40,8 @@ class PuzzleMapLayer: CAShapeLayer {
         }
     }
     
+    private var textFont: UIFont!
+    
     override init() {
         super.init()
         commonInit()
@@ -44,12 +52,29 @@ class PuzzleMapLayer: CAShapeLayer {
     }
     
     private func commonInit() {
+        fillColor = UIColor.black.cgColor
         strokeColor = UIColor.black.cgColor
+        textFont = UIDevice.current.userInterfaceIdiom == .pad ? UIFont.systemFont(ofSize: 38.0) : UIFont.systemFont(ofSize: 28.0)
     }
     
     
     fileprivate var rowPoints = [CGFloat]()
     fileprivate var columPoints = [CGFloat]()
+    fileprivate var retangles = [RetangleValue]()
+    
+    private func retangleFrame(at index: Int) -> CGRect {
+        guard rows > 0 && colums > 0 else {
+            return CGRect()
+        }
+        let i = index / colums
+        let j = index % colums
+        
+        let frame = self.bounds
+        let width = frame.size.width - lineWidth/2
+        let height = frame.size.height - lineWidth/2
+        
+        return CGRect(x: CGFloat(j) * width/CGFloat(colums), y: CGFloat(i) * height/CGFloat(rows), width: width/CGFloat(colums), height: height/CGFloat(rows))
+    }
     
     private func updatePoints() {
         rowPoints.removeAll()
@@ -57,9 +82,10 @@ class PuzzleMapLayer: CAShapeLayer {
         if rows == 0 || colums == 0 {
             return
         }
-        let frame = self.bounds
-        let width = frame.size.width - lineWidth/2
-        let height = frame.size.height - lineWidth/2
+        
+        let width = bounds.size.width - lineWidth/2
+        let height = bounds.size.height - lineWidth/2
+        
         rowPoints.append(0)
         for i in stride(from: lineWidth/2, through: height, by: height/CGFloat(rows)) {
             rowPoints.append(i)
@@ -73,7 +99,7 @@ class PuzzleMapLayer: CAShapeLayer {
         columPoints.append(width + lineWidth/2)
     }
     
-    private func updatePath() {
+    fileprivate func updatePath() {
         let width = bounds.size.width
         let height = bounds.size.height
         let linePath = UIBezierPath();
@@ -85,13 +111,69 @@ class PuzzleMapLayer: CAShapeLayer {
             linePath.move(to: CGPoint(x: x, y: 0))
             linePath.addLine(to: CGPoint(x: x, y: height))
         }
+        
+        // retangles
+        for i in 0..<retangles.count {
+            let retangle = retangles[i]
+            let frame = retangleFrame(at: i)
+            let retanglePath = UIBezierPath(rect: frame)
+            switch retangle {
+            case .black:
+                retanglePath.fill()
+                linePath.append(retanglePath)
+            default:
+                break
+            }
+        }
+        
         path = linePath.cgPath
+    }
+    
+    override func draw(in ctx: CGContext) {
+        super.draw(in: ctx)
+        UIGraphicsPushContext(ctx)
+        
+        for i in 0..<retangles.count {
+            let retangle = retangles[i]
+            let frame = retangleFrame(at: i)
+            switch retangle {
+            case .value(let num):
+                let label = UILabel(frame: frame)
+                label.font = textFont
+                label.baselineAdjustment = .alignCenters
+                label.adjustsFontSizeToFitWidth = true
+                label.textAlignment = .center
+                label.text = String(num)
+                label.drawText(in: frame)
+            default:
+                break
+            }
+        }
+        UIGraphicsPopContext()
+        
     }
     
     func updateMap() {
         updatePoints()
         updatePath()
         setNeedsDisplay()
+    }
+    
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        updateMap()
+    }
+    
+    func retangleIndex(at pos: CGPoint) -> Int {
+        
+        debugPrint("tap pos: \(pos)")
+        let width = bounds.size.width - lineWidth/2
+        let height = bounds.size.height - lineWidth/2
+        //debugPrint("width=\(height/CGFloat(rows)), height=\(width/CGFloat(colums))")
+        let i = Int((pos.y - lineWidth/2) / (height/CGFloat(rows)))
+        let j = Int((pos.x - lineWidth/2) / (width/CGFloat(colums)))
+        debugPrint("(\(i), \(j))")
+        return i * colums + j
     }
     
 }
@@ -137,6 +219,61 @@ class PuzzleMapLayer: CAShapeLayer {
             else {
                 colums = 0
             }
+            // update layers retangles
+            puzzleLayer.retangles.removeAll()
+            for arr in puzzleArray {
+                for a in arr {
+                    if a == -1 {
+                        puzzleLayer.retangles.append(.black)
+                    }
+                    else if a == 0 {
+                        puzzleLayer.retangles.append(.white)
+                    }
+                    else {
+                        puzzleLayer.retangles.append(.value(a))
+                    }
+                }
+            }
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        tapGR.numberOfTapsRequired = 1
+        tapGR.numberOfTouchesRequired = 1
+        self.addGestureRecognizer(tapGR)
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            // handling code
+            let pos = sender.location(in: self)
+            let index = puzzleLayer.retangleIndex(at: pos)
+            
+            let value = puzzleArray[index/colums][index % colums]
+            if value > 0 {
+                return
+            }
+            if value == -1 {
+                puzzleArray[index/colums][index % colums] = 0
+                puzzleLayer.retangles[index] = .white
+            }
+            else {
+                puzzleArray[index/colums][index % colums] = -1
+                puzzleLayer.retangles[index] = .black
+            }
+            puzzleLayer.updatePath()
+            puzzleLayer.setNeedsDisplay()
         }
     }
 
--- Nurikabe/ViewController.swift
+++ Nurikabe/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var puzzleMapView: PuzzleMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // test
+        puzzleMapView.puzzleArray = [
+            [2, 0, 0, 0, 0, 6],
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 2],
+            [0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+            [0, 0, 3, 0, 0, 1]
+        ]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
